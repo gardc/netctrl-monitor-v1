505ba8e0c8ca293a02817a6dbdb07dd46b9955c9
gardc <gard@cappelendigital.no>
2022-03-27 11:42:51 -0700

    trying to find bug on linux

--- networking/scan/scan_test.go
+++ networking/scan/scan_test.go
@@ -1,17 +1,29 @@
 package scan
 
 import (
-	"github.com/google/gopacket/pcap"
 	"net"
-	"netctrl.io/monitor/networking/network"
 	"os"
+	"os/exec"
+	"runtime"
 	"testing"
 	"time"
+
+	"github.com/google/gopacket/pcap"
+	"netctrl.io/monitor/networking/network"
 )
 
 func TestArpScan(t *testing.T) {
 	if os.Getenv("CI") != "" {
 		t.Skip("Skip ARP Scan in CI")
+	}
+	if runtime.GOOS == "linux" {
+		os.Setenv("devmode", "dev")
+		cmd := exec.Command("/usr/bin/pkexec", "setcap", "cap_net_raw,cap_net_admin=eip", os.Args[0])
+		o, err := cmd.Output()
+		t.Logf("arg0: %s\nsetcap out: %s\nsetcap err: %v\n", os.Args[0], o, err)
+		// e, err := os.Executable()
+		// t.Logf("executable: %v, err: %v\n", e, err)
+		time.Sleep(500)
 	}
 
 	start := time.Now()
@@ -20,6 +32,7 @@ func TestArpScan(t *testing.T) {
 
 	localIP, _ := network.GetLocalIP()
 	iface, _ := network.GetLocalIfaceFromIP(localIP)
+	t.Logf("ip: %v, iface: %v\n", localIP, iface)
 
 	// Open up a pcap handle for packet reads/writes.
 	handle, err := pcap.OpenLive(iface.Name, 65536, true, pcap.BlockForever)
@@ -38,7 +51,8 @@ func TestArpScan(t *testing.T) {
 		Mask: net.CIDRMask(24, 32),
 	}
 
-	go ARPScan(handle, iface, localIPNet, timeout, "", results, done, stop)
+	jwt := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQG5ldGN0cmwuaW8iLCJwcm9Vc2VyIjp0cnVlLCJpYXQiOjE2NDgzOTk3MDl9.Xufgc6HOdahG81_6vUlSHfu2LpT7gH7bUFHwa1wq6s8"
+	go ARPScan(handle, iface, localIPNet, timeout, jwt, results, done, stop)
 
 	// Quit after 15 seconds
 	timesUp := make(chan struct{})
