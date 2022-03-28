87cd2ee607dbbd2d8f25c6572909975aff2da836
gardc <gard@cappelendigital.no>
2022-03-29 00:47:42 +0200

    github actions fix

--- frontend/src/Pages/Settings.jsx
+++ frontend/src/Pages/Settings.jsx
@@ -46,6 +46,7 @@ const SettingsPage = () => {
         getVersion().then((r) => {
             setVersion(r);
         });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     useEffect(() => {
