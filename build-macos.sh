21b6c2cef92bbaec9741889dd0daa939ac504916
gardc <gard@cappelendigital.no>
2022-03-30 19:15:19 +0200

    github actions

--- build-macos.sh
+++ build-macos.sh
@@ -1,7 +1,7 @@
 echo "building on AMD64"
 wails build -platform darwin/amd64 -clean
 echo "Signing Package"
-./gon -log-level=info ./build/darwin/gon-sign.json
+gon -log-level=info ./build/darwin/gon-sign.json
 echo "Zipping Package"
 ditto -c -k --keepParent ./build/bin/Monitor.app ./netctrl-monitor-darwin-amd64.zip
 echo "Cleaning up"
@@ -9,10 +9,10 @@ rm -rf ./build/bin/NetCTRL-Monitor.app
 echo "building on ARM64"
 wails build -platform darwin/arm64 -clean
 echo "Signing Package"
-./gon -log-level=info -log-json ./build/darwin/gon-sign.json
+gon -log-level=info -log-json ./build/darwin/gon-sign.json
 echo "Zipping Package"
 ditto -c -k --keepParent ./build/bin/Monitor.app ./netctrl-monitor-darwin-arm64.zip
 echo "Cleaning up"
 rm -rf ./build/bin/Monitor.app
 echo "Notarizing Zip Files"
-./gon -log-level=info ./build/darwin/gon-notarize.json
+gon -log-level=info ./build/darwin/gon-notarize.json
